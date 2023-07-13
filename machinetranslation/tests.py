ff1fcecdd297e10af44dc47cc0ccfa3a2e932b3e
GD <henriqueaf22@gmail.com>
2023-07-13 17:53:46 -0300

    Commit mades

--- machinetranslation/tests.py
+++ machinetranslation/tests.py
@@ -4,10 +4,12 @@ from translator import english_to_french, french_to_english
 
 class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
-        self.assertEqual(english_to_french("Hello"), "Bonjour")
-        self.assertEqual(english_to_french("Mister"), "Monsieur")
+        self.assertEqual(english_to_french('hello'), 'bonjour')
+        self.assertEqual(english_to_french('ball'), 'ballon')
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
-        self.assertEqual(french_to_english("Bonjour"), "Hello")
-        self.assertEqual(french_to_english("Monsieur"), "Mister")
+        self.assertEqual(french_to_english('bonjour'), 'hello')
+        self.assertEqual(french_to_english('balle'), 'bullet')
+
+unittest.main()
--- machinetranslation/translator.py
+++ machinetranslation/translator.py
@@ -2,9 +2,12 @@
 from deep_translator import MyMemoryTranslator
 
 def english_to_french(englishText):
-
+    frenchText = MyMemoryTranslator("en-US", "fr-FR").translate(englishText, True)
+    
     return frenchText
 
 def french_to_english(frenchText):
+    
+    englishText = MyMemoryTranslator("fr-FR", "en-US").translate(frenchText, True)
 
     return englishText
